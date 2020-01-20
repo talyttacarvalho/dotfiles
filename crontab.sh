9ae7961aaae748031a21b194f44d8cc598ff3cd9
Lucas Marques <lucasmarques73@hotmail.com>
2020-01-20 18:34:21 -0300

    Preparando arquivos para instalacao de apps

---
+++ crontab.sh
@@ -0,0 +1,7 @@
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "*/15 * * * * /home/lucas/.unsplash/unsplash.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
---
+++ unsplash/unsplash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER="willianjusten"
+
+rm /home/lucas/.unsplash/wallpaper.jpg
+wget -O /home/lucas/.unsplash/wallpaper.jpg https://source.unsplash.com/user/$USER/1920x1080
+gsettings set org.gnome.desktop.background picture-uri file:///home/lucas/.unsplash/wallpaper.jpg
+
+# sudo chmod +x .unsplash.sh
+# ./.unsplash.sh
+
+
+# crontab -e
+# https://crontab.guru/#*_*/20_*_*_*
+# * */20 * * * /home/lucas/.unsplash.unsplash.sh
+
+# */15 * * * * /home/lucas/.unsplash/unsplash.sh
