fade7ec1105e613a13768715aed1abb87818f355
Lucas Marques <lucasmarques73@hotmail.com>
2020-01-30 16:24:36 -0300

    Alterando tela de bloqueio

--- unsplash/unsplash.sh
+++ unsplash/unsplash.sh
@@ -5,6 +5,7 @@ USER="willianjusten"
 rm /home/lucas/.unsplash/wallpaper.jpg
 wget -O /home/lucas/.unsplash/wallpaper.jpg https://source.unsplash.com/user/$USER/1920x1080
 gsettings set org.gnome.desktop.background picture-uri file:///home/lucas/.unsplash/wallpaper.jpg
+gsettings set org.gnome.desktop.screensaver picture-uri file:///home/lucas/.unsplash/wallpaper.jpg
 
 # sudo chmod +x .unsplash.sh
 # ./.unsplash.sh
