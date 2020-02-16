22e150d352fd5618de07632f7dc38f55a53c903b
Talytta Carvalho <talytta110@hotmail.com>
2020-02-16 18:26:14 -0300

    Plugins e tema zsh

---
+++ zsh/theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $HOME
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
